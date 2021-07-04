3428d846038c44f26e574889cef36d0161d758e4
VoduDay <andriy.maniak@gmail.com>
2021-07-04 19:46:53 +0300

    add images

--- index.js
+++ index.js
@@ -11,3 +11,38 @@ app.use(express.static('public'))
 const server = app.listen(port, () => {
     console.log(`server is running on port ${port}`);
 });
+
+const io = socket(server, {
+    cors: {
+        origin: '*',
+    }
+});
+
+const contacts = [
+    {
+        name: 'echo bot',
+        image: '',
+    },
+    {
+        name: 'spam bot',
+        image: '',
+    }
+];
+
+io.on('connection', socket => {
+    console.log(socket.id)
+    socket.join('messenger');
+    console.log(`user joined messenger`);
+
+    io.to('messenger').emit('receive_list_of_contacts', contacts);
+
+    socket.on('join_room', data => {
+        socket.join(data);
+        console.log(`User joined room: ${data}`);
+    });
+
+    socket.on('disconnect', reason => {
+        console.log('User disconnected');
+        console.log(reason);
+    });
+});
