df817e4d1d325d28bf2198c904af4e0385879f96
VoduDay <andriy.maniak@gmail.com>
2021-07-05 21:44:45 +0300

    update connection event listener

--- events/eventListeners/connection.js
+++ events/eventListeners/connection.js
@@ -4,29 +4,28 @@ const User = require('../../entities/User');
 const connection = (io, contacts) => {
     io.on('connection', socket => {
         let currentUser;
+        socket.join('messenger');
 
-        socket.on('join_messenger', userData => {
-            socket.join('messenger');
+        const user = socket.handshake.query;
 
-            if (userData) {
-                currentUser = contacts.find(contact => contact.id === userData.id);
-            }
+        if (user.id) {
+            currentUser = contacts.find(contact => contact.id === user.id);
+        }
 
-            if (currentUser) {
-                currentUser.goOnline();
-            } else {
-                const newUser = new User();
-                newUser.goOnline();
-                currentUser = newUser;
-                contacts.push(newUser);
-                socket.emit('receive_created_user_info', newUser);
-            }
+        if (currentUser) {
+            currentUser.goOnline();
+        } else {
+            const newUser = new User(socket.id)
+            currentUser = newUser;
+            currentUser.goOnline();
+            contacts.push(newUser);
+            socket.emit('receive_created_user_info', newUser);
+        }
 
-            console.log(currentUser)
-            io.to('messenger').emit('receive_list_of_contacts', contacts.filter(contact => contact.id !== currentUser.id));
-        });
+        io.to('messenger').emit('receive_list_of_contacts', contacts.filter(contact => contact.id !== currentUser.id));
 
-        disconnect();
+
+        disconnect(socket, io, contacts);
     });
 };
 
